889fa7cbb9b706f0222ae3d9fbfa5895203b25fe
Yash Tiwari <me.yashtiwari@gmail.com>
2026-04-29 17:14:26 +0530

    feat: add new components for blog, contact, footer, header, home, journey, project-card, project-detail, and projects with corresponding templates and styles

--- apps/frontend/src/app/app.config.ts
+++ apps/frontend/src/app/app.config.ts
@@ -2,17 +2,19 @@ import {
   ApplicationConfig,
   provideBrowserGlobalErrorListeners,
 } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withViewTransitions } from '@angular/router';
 import { appRoutes } from './app.routes';
 import {
   provideClientHydration,
   withEventReplay,
 } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideClientHydration(withEventReplay()),
     provideBrowserGlobalErrorListeners(),
-    provideRouter(appRoutes),
+    provideRouter(appRoutes, withViewTransitions()),
+    provideHttpClient(),
   ],
 };
---
+++ apps/frontend/src/app/pages/journey/journey.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-journey',
+  imports: [],
+  templateUrl: './journey.html',
+  styleUrl: './journey.css',
+})
+export class Journey {}
